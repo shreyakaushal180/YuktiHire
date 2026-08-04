36dbec08890e6103c8630aadb0e0ba75f605ea42
Shreya Kaushal <shreyakaushal118@gmail.com>
2026-08-04 23:50:38 +0530

    Fix Express CORS options wildcard path

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -20,7 +20,7 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
-app.options("*", cors(corsOptions))
+app.options("/*", cors(corsOptions))
 
 /* require all routes*/
 const authRouter = require("./routes/auth.routes")
